a45fc2ada9802c42324aafff8e329a3ce237f87e
Mohd Rizwan <rizwan@involve.asia>
2022-06-15 09:35:19 +0800

    add login palceholder

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,9 +17,9 @@
 
             <!-- Email Address -->
             <div>
-                <x-label for="email" :value="__('Email')" />
+                <x-label for="email" value="demo@nech.zone" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" value="demo1234" required autofocus />
             </div>
 
             <!-- Password -->
